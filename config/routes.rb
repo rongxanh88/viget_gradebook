1ae226fdf66c32600d7cc884fdfded51c307d825
rongxanh88 <rongxanh88@gmail.com>
2017-10-06 13:12:11 -0600

    add login/logout routes as well as student dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: "home#index"
+
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
+
+  namespace :student do
+    resources :dashboard, only: [:index]
+  end
 end
