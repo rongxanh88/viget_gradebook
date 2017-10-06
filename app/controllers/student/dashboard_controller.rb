cce821fddbe1212f6b030f0f0cd2246802cb8e0c
rongxanh88 <rongxanh88@gmail.com>
2017-10-06 14:28:34 -0600

    student logs into dashboard spec tests passing

--- app/controllers/student/dashboard_controller.rb
+++ app/controllers/student/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Student::DashboardController < Student::AuthorizationController
   
   def index
+    @course_grades = Course.courses_enrolled(current_user)
+    @semester_gpa = Enrollment.semester_gpas
   end
 
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,4 +3,14 @@ class Enrollment < ApplicationRecord
   
   belongs_to :user
   belongs_to :semester_course
+
+  def self.semester_gpas
+    Enrollment.joins("INNER JOIN (" +
+              Semester.joins(:semester_courses)
+                      .select("semester_courses.id AS id, semesters.name AS name")
+                      .to_sql +
+                      ") sem_courses ON sem_courses.id = enrollments.semester_course_id"
+                      ).select("sem_courses.name AS semester, AVG(enrollments.grade) AS GPA")
+                      .group("semester")
+  end
 end
